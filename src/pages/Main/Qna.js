42fa0a7e8720b05f963dfc094158ece06b0ad597
suminllll <79704928+suminllll@users.noreply.github.com>
2022-02-14 22:36:41 +0900

    search 작업중

--- src/pages/Main/Qna.js
+++ src/pages/Main/Qna.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Qna = () => {
   const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const day = now.getDate();
+  let today = `${year}-${month}-${day}`;
 
   useEffect(() => {
     fetch('data/boardData.json')
@@ -12,8 +20,16 @@ const Qna = () => {
       });
   }, []);
 
+  const handleTitle = () => {
+    navigate('/');
+  };
+
   return (
     <QnaBox>
+      <SearchBox>
+        <Search placeholder="Search" />
+        {/* <FontAwesomeIcon icon={faSearch} className="search" /> */}
+      </SearchBox>
       <Table>
         <thead>
           <tr>
@@ -28,18 +44,42 @@ const Qna = () => {
             return (
               <tr key={data.no}>
                 <BodyTd>{data.no}</BodyTd>
-                <BodyTd style={{ textAlign: 'left' }}>{data.title}</BodyTd>
+                <BodyTd
+                  onClick={handleTitle}
+                  style={{ textAlign: 'left', cursor: 'pointer' }}
+                >
+                  {data.title}
+                </BodyTd>
                 <BodyTd>{data.writer}</BodyTd>
-                <BodyTd>{data.date}</BodyTd>
+                <BodyTd>{today}</BodyTd>
               </tr>
             );
           })}
         </tbody>
       </Table>
+      <Button>글쓰기</Button>
     </QnaBox>
   );
 };
 
+const SearchBox = styled.div`
+  float: right;
+  margin-bottom: 20px;
+  width: 20%;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Search = styled.input`
+  border-style: none;
+  padding-bottom: 5px;
+
+  ::placeholder {
+    color: lightgray;
+    letter-spacing: 1px;
+  }
+`;
+
+// const FontAwesomeIcon = styled.div``;
 const QnaBox = styled.div`
   margin: 4%;
   height: 100vh;
@@ -53,6 +93,8 @@ const Table = styled.table`
 const HeadTh = styled.th`
   padding: 15px 0;
   color: #003300;
+  letter-spacing: 1px;
+  font-weight: 400;
 `;
 
 const BodyTd = styled.td`
@@ -61,5 +103,21 @@ const BodyTd = styled.td`
   border-top: 1px solid lightgray;
   border-bottom: 1px solid lightgray;
   color: lightgray;
+  letter-spacing: 1px;
 `;
+
+const Button = styled.button`
+  float: right;
+  margin-top: 15px;
+  width: 85px;
+  height: 30px;
+  border-style: none;
+  background-color: #003300;
+  opacity: 0.7;
+  color: white;
+  font-size: 14px;
+  letter-spacing: 1px;
+  cursor: pointer;
+`;
+
 export default Qna;
